d23bc95a3f07c6b4a7cd44fd7f4bb878d8467885
ImanAdithya <adithyaiman07@gmail.com>
2024-01-25 09:25:12 +0530

    content updated

--- src/views/components/Header/Header.tsx
+++ src/views/components/Header/Header.tsx
@@ -1,17 +1,36 @@
 import {Component} from "react";
 import logo from '../../assets/logo.png'
 
-export class Header extends Component {
+interface HeaderProps {}
+
+interface HeaderState {
+    isSidebarOpen: boolean;
+}
+
+export class Header extends Component<HeaderProps, HeaderState> {
+    constructor(props: HeaderProps) {
+        super(props);
+        this.state = {
+            isSidebarOpen: false,
+        };
+    }
+
+    toggleSidebar = () => {
+        this.setState((prevState) => ({
+            isSidebarOpen: !prevState.isSidebarOpen,
+        }));
+    };
+
     render() {
         return (
-            <div className='w-full  px-72 py-5 overflow-x-hidden fixed  z-50 top-0 left-0 ' >
-                <div className='flex flex-row justify-between'>
+            <div className='w-full px-20 xl:px-72 py-5 overflow-x-hidden fixed  z-50 top-0 left-0 ' >
+                <div className='flex flex-row justify-center xl:justify-between'>
                         <div className='flex flex-row items-center'>
                             <img src={logo} className=' h-12'/>
                         </div>
 
-                        <div className='flex flex-row items-center text-white gap-24'>
-                            <ul className='flex gap-10 text-[1rem]'>
+                        <div className=' flex-row items-center text-white gap-24 hidden 2xl:flex'>
+                            <ul className='flex gap-10 text-[0.8vw]'>
                                 <li >Home</li>
                                 <li >About Us</li>
                                 <div >
@@ -94,8 +113,38 @@ export class Header extends Component {
                             </div>
                         </div>
 
+                    <button
+                        data-drawer-target="default-sidebar"
+                        data-drawer-toggle="default-sidebar"
+                        aria-controls="default-sidebar"
+                        type="button"
+                        onClick={this.toggleSidebar}
+                        className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg xl:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    >
+                        <span className="sr-only">Open sidebar</span>
+                        <svg
+                            className="w-6 h-6"
+                            aria-hidden="true"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+                        </svg>
+                    </button>
 
                 </div>
+
+                {this.state.isSidebarOpen && (
+                    <div className='h-[100vw] w-full bg-transparent xl:hidden flex justify-center '>
+                        <ul className='text-white text-lg lg:text-xl opacity-65 flex flex-row gap-5 mt-5'>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Service</li>
+                            <li>Contact</li>
+                        </ul>
+                    </div>
+                )}
 
             </div>
         );
